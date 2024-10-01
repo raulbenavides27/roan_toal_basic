df6b83c1e22377f267b1ec2287dd125231982c66
raulbenavides27 <ra.benavides@duocuc.cl>
2024-10-01 20:23:33 -0300

    .js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,25 @@
 const imagen1 = document.getElementById('imagen1');
 const imagen2 = document.getElementById('imagen2');
 const imagen3 = document.getElementById('imagen3');
-
+const texto1 = document.getElementById('texto1');
 const cargarImagen = (entradas, observador) => {
     entradas.forEach((entrada) => {
         if (entrada.isIntersecting) {
-            entrada.target.classList.add('visible'); 
+            entrada.target.classList.add('visible');
+            console.log(`Imagen ${entrada.target.id} visible`); // Para verificar
         } else {
-            entrada.target.classList.remove('visible'); 
+            entrada.target.classList.remove('visible');
         }
     });
-}
+};
 
 const observador = new IntersectionObserver(cargarImagen, {
     root: null,
     rootMargin: '0px',
-    threshold: 0.5 // Cambiado para mejor comportamiento
+    threshold: 0.1
 });
 
 observador.observe(imagen1);
 observador.observe(imagen2);
 observador.observe(imagen3);
+observador.observe(texto1);
