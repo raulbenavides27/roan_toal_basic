4e0fb6cadaf0210889737bab408d98163372d0b4
raulbenavides27 <ra.benavides@duocuc.cl>
2024-10-02 17:35:01 -0300

    pruebas

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@ const imagen1 = document.getElementById('imagen1');
 const imagen2 = document.getElementById('imagen2');
 const imagen3 = document.getElementById('imagen3');
 const texto1 = document.getElementById('texto1');
+const imagen4 = document.getElementById('imagen4');
+const imagen5 = document.getElementById('imagen5');
+const imagen6 = document.getElementById('imagen6');
 const cargarImagen = (entradas, observador) => {
     entradas.forEach((entrada) => {
         if (entrada.isIntersecting) {
@@ -23,3 +26,6 @@ observador.observe(imagen1);
 observador.observe(imagen2);
 observador.observe(imagen3);
 observador.observe(texto1);
+observador.observe(imagen4)
+observador.observe(imagen5)
+observador.observe(imagen6)
